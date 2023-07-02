11ad1f548c5c1dad23ee2bf0e25f650ad23248b3
angelinetipa <angelinetipa@gmail.com>
2023-07-02 15:34:43 +0800

    pseudocode for test fan

---
+++ TestFan.py
@@ -0,0 +1,5 @@
+# First object, assign the maximum speed, radius 10, color yellow, and turn it on
+# Display first object speed, radius, color, on properties
+
+# Secpnd object, assign the medium speed, radius 5, color blue, and turn it off
+# Dsiplay second object speed, radius, color, on properties

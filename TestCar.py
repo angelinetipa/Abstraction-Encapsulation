cc0388bfb8b9f65a98001f21fcd3cf650e23ac2c
angelinetipa <angelinetipa@gmail.com>
2023-07-02 20:59:15 +0800

    last changed for car class

--- TestCar.py
+++ TestCar.py
@@ -8,12 +8,12 @@ my_car = Car(2021, "Honda Civic Hatchback")
 for number in range(5):
     my_car.accelerate()
     # get the current speed of the car and display it
-    print(f"""{Style.BRIGHT}\n{my_car.get_make()} {my_car.get_year()} {Style.RESET_ALL} {Fore.BLACK}>>  Accelerated by {number + 1} times {Style.RESET_ALL}
+    print(f"""{Style.BRIGHT}\n{my_car.get_make()} {my_car.get_year()} {Style.RESET_ALL} {Fore.BLACK}>>  Accelerated by {number + 1} time/s {Style.RESET_ALL}
 {Style.BRIGHT}▪ Speed: {Style.RESET_ALL}{my_car.get_speed()}""")
     
 # call the brake method five times
-for number in range(6):
+for number in range(5):
     my_car.brake()
     # get the current speed of the car and display it
-    print(f"""{Style.BRIGHT}\n{my_car.get_make()} {my_car.get_year()} {Style.RESET_ALL} {Fore.BLACK}>>  Decelerated by {number + 1} times {Style.RESET_ALL}
+    print(f"""{Style.BRIGHT}\n{my_car.get_make()} {my_car.get_year()} {Style.RESET_ALL} {Fore.BLACK}>>  Decelerated by {number + 1} time/s {Style.RESET_ALL}
 {Style.BRIGHT}▪ Speed: {Style.RESET_ALL}{my_car.get_speed()}""")

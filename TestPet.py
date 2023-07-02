a111fef30010d17ba2a1b14cfd5d0f98a79ac9ab
angelinetipa <angelinetipa@gmail.com>
2023-07-02 21:40:50 +0800

    pseudocode for test pet

---
+++ TestPet.py
@@ -0,0 +1,3 @@
+# import pet class
+# creates an object of the class and prompts the user to enter the name, type, and age of his or her pet
+# retrieve the pet’s name, type, and age and display this data on the screen

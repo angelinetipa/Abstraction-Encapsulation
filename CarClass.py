9b0cef5e2e5d7b4b64288e64806e1628c562b863
angelinetipa <angelinetipa@gmail.com>
2023-07-02 18:40:03 +0800

    def brake

--- CarClass.py
+++ CarClass.py
@@ -8,9 +8,12 @@ class Car:
         self.__make = make
         # _ _speed (for the car’s current speed)
         self.__speed = 0
+
     # accelerate method should add 5 to the speed data attribute each time it is called
     def accelerate(self):
         self.__speed += 5
       
-# brake method should subtract 5 from the speed data attribute each time it is called
+    # brake method should subtract 5 from the speed data attribute each time it is called
+    def brake(self):
+        self.__speed -= 5
 # get_speed method should return the current speed

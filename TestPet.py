8c3686d71011bd52410ebede11e3612a4edddc79
angelinetipa <angelinetipa@gmail.com>
2023-07-02 21:42:32 +0800

    import pet class, colorama

--- TestPet.py
+++ TestPet.py
@@ -1,3 +1,6 @@
 # import pet class
+from PetClass import Pet
+from colorama import Style, Fore
+
 # creates an object of the class and prompts the user to enter the name, type, and age of his or her pet
 # retrieve the pet’s name, type, and age and display this data on the screen

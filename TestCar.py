13e0ecb10ec2aeb8d8a9309115b63f08a6299584
angelinetipa <angelinetipa@gmail.com>
2023-07-02 19:23:55 +0800

    car object

---
+++ TestCar.py
@@ -0,0 +1,9 @@
+# import car class
+from CarClass import Car
+
+# creates a Car object
+your_car = Car(2021, "Honda Civic Hatchback")
+# calls the accelerate method five times
+# After each call to the accelerate method, get the current speed of the car and display it
+# call the brake method five times
+# After each call to the brake method, get the current speed of the car and display it.

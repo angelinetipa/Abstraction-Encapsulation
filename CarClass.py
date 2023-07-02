cfe0bd90d8429da62b78f7b808dcfa859d69db65
angelinetipa <angelinetipa@gmail.com>
2023-07-02 18:38:54 +0800

    change self speed

--- CarClass.py
+++ CarClass.py
@@ -1,13 +1,13 @@
 # class named Car
 class Car:
     # constructor that accepts the car’s year model, make as arguments, assign 0 to the speed 
-    def __init__(self, year, make, speed = 0):
+    def __init__(self, year, make):
         # _ _year_model (for the car’s year model)
         self.__year_model = year
         # _ _make (for the make of the car)
         self.__make = make
         # _ _speed (for the car’s current speed)
-        self.__speed = speed
+        self.__speed = 0
     # accelerate method should add 5 to the speed data attribute each time it is called
     def accelerate(self):
         self.__speed += 5

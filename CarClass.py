d8417c80159ddb43a5cf5efe399328f2e4beeb17
angelinetipa <angelinetipa@gmail.com>
2023-07-02 18:29:59 +0800

    self make

--- CarClass.py
+++ CarClass.py
@@ -5,6 +5,7 @@ class Car:
         # _ _year_model (for the car’s year model)
         self.__year_model = year
         # _ _make (for the make of the car)
+        self.__make = make
         # _ _speed (for the car’s current speed)
 # accelerate method should add 5 to the speed data attribute each time it is called
 # brake method should subtract 5 from the speed data attribute each time it is called

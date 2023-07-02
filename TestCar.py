209ecbf8359025d6ef3b0148b849d0ea5e852d32
angelinetipa <angelinetipa@gmail.com>
2023-07-02 19:31:11 +0800

    changed car objects name

--- TestCar.py
+++ TestCar.py
@@ -2,8 +2,10 @@
 from CarClass import Car
 
 # creates a Car object
-your_car = Car(2021, "Honda Civic Hatchback")
+my_car = Car(2021, "Honda Civic Hatchback")
 # calls the accelerate method five times
-# After each call to the accelerate method, get the current speed of the car and display it
+for every_num in range(5):
+    my_car.accelerate()
+    # get the current speed of the car and display it
 # call the brake method five times
 # After each call to the brake method, get the current speed of the car and display it.

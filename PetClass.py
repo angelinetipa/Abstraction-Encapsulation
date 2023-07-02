b66d6aa521b6cc2a163d6bfcc089cd4fa79dcda2
angelinetipa <angelinetipa@gmail.com>
2023-07-02 21:18:09 +0800

    constructor

--- PetClass.py
+++ PetClass.py
@@ -1,9 +1,10 @@
 # class named Pet
 class Pet:
-# _ _init_ _ method that creates these attributes (name, animal type, age)
-# _ _name (for the name of a pet)
-# _ _animal_type (for the type of animal that a pet is. Example values are ‘Dog’, ‘Cat’, and ‘Bird’)
-# _ _age (for the pet’s age)
+    # _ _init_ _ method that creates these attributes (name, animal type, age)
+    def __init__(self, name, type, age):
+    # _ _name (for the name of a pet)
+    # _ _animal_type (for the type of animal that a pet is. Example values are ‘Dog’, ‘Cat’, and ‘Bird’)
+    # _ _age (for the pet’s age)
 # set_name() method assigns a value to the _ _name field
 # set_animal_type() method assigns a value to the _ _animal_type field
 # set_age() method assigns a value to the _ _age field

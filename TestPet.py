36fa0dce5601e90a2f6f0b74b8953bb1be5e27ef
angelinetipa <angelinetipa@gmail.com>
2023-07-02 22:22:41 +0800

    added space under

--- TestPet.py
+++ TestPet.py
@@ -8,6 +8,6 @@ users_pet = Pet(pet_name, pet_animal, pet_age)
 
 # retrieve the pet’s name, type, and age and display this data on the screen
 print(f"""{Style.BRIGHT}\nUSER'S PET INFORMATION {Style.RESET_ALL} {Style.BRIGHT}
-      ▪ Name: {Style.RESET_ALL}{users_pet.get_name()} {Style.BRIGHT}
-      ▪ Type of Animal: {Style.RESET_ALL}{users_pet.get_animal_type()} {Style.BRIGHT}
-      ▪ Age: {Style.RESET_ALL}{users_pet.get_age()}""")
+▪ Name: {Style.RESET_ALL}{users_pet.get_name()} {Style.BRIGHT}
+▪ Type of Animal: {Style.RESET_ALL}{users_pet.get_animal_type()} {Style.BRIGHT}
+▪ Age: {Style.RESET_ALL}{users_pet.get_age()}\n""")

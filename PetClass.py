f67a331658798abe57e306116b0e7a3be37e6c71
angelinetipa <angelinetipa@gmail.com>
2023-07-02 21:33:44 +0800

    def set_age

--- PetClass.py
+++ PetClass.py
@@ -16,6 +16,8 @@ class Pet:
     def set_animal_type(self, animal):
         self.__animal_type = animal
     # set_age() method assigns a value to the _ _age field
+    def set_age(self, age):
+        self.__age = age
     # get_name() method returns the value of the _ _ name field
     # get_animal_type() method returns the value of the _ _animal_type field
     # get_age() method returns the value of the _ _age field

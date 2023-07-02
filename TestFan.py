c2f51a1b6acc84b66f8b95a5d7b7bc811b3cf746
angelinetipa <angelinetipa@gmail.com>
2023-07-02 15:39:41 +0800

    first fan

--- TestFan.py
+++ TestFan.py
@@ -2,6 +2,11 @@
 from FanClass import Fan
 
 # First object, assign the maximum speed, radius 10, color yellow, and turn it on
+fan1 = Fan()
+fan1.set_speedFAST()
+fan1.set_radius(10)
+fan1.set_color("yellow")
+fan1.set_fanON()
 # Display first object speed, radius, color, on properties
 
 # Secpnd object, assign the medium speed, radius 5, color blue, and turn it off

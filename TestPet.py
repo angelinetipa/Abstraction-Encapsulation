cca19efd27e462d5ee426925b279a7b5ac25a188
angelinetipa <angelinetipa@gmail.com>
2023-07-02 22:32:35 +0800

    changed display

--- TestPet.py
+++ TestPet.py
@@ -3,11 +3,12 @@ from PetClass import Pet
 from colorama import Style, Fore
 
 # creates an object of the class and prompts the user to enter the name, type, and age of his or her pet
-pet_name, pet_animal, pet_age = input("(Add space between them.)\nEnter your pet's name, type and age: ").split()
+pet_name, pet_animal, pet_age = input(f"""\n(Add space between your answer){Style.BRIGHT}
+Enter your pet's name, type and age: {Style.RESET_ALL}""").split()
 users_pet = Pet(pet_name.capitalize(), pet_animal.capitalize(), pet_age)
 
 # retrieve the pet’s name, type, and age and display this data on the screen
 print(f"""{Style.BRIGHT}\nUSER'S PET INFORMATION {Style.RESET_ALL} {Style.BRIGHT}
-▪ Name: {Style.RESET_ALL}{users_pet.get_name()} {Style.BRIGHT}
-▪ Type of Animal: {Style.RESET_ALL}{users_pet.get_animal_type()} {Style.BRIGHT}
-▪ Age: {Style.RESET_ALL}{users_pet.get_age()}\n""")
+▪ Name: {Fore.BLACK} {users_pet.get_name()} {Style.RESET_ALL} {Style.BRIGHT}
+▪ Type of Animal: {Fore.BLACK} {users_pet.get_animal_type()} {Style.RESET_ALL} {Style.BRIGHT}
+▪ Age: {Fore.BLACK} {users_pet.get_age()}\n{Style.RESET_ALL}""")

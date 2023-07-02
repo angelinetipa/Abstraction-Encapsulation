ff372b380a68c75713449bbb2c91c8e9fc723551
angelinetipa <angelinetipa@gmail.com>
2023-07-02 22:23:44 +0800

    capitalized first letter

--- TestPet.py
+++ TestPet.py
@@ -4,7 +4,7 @@ from colorama import Style, Fore
 
 # creates an object of the class and prompts the user to enter the name, type, and age of his or her pet
 pet_name, pet_animal, pet_age = input("(Add space between them.)\nEnter your pet's name, type and age: ").split()
-users_pet = Pet(pet_name, pet_animal, pet_age)
+users_pet = Pet(pet_name.capitalize(), pet_animal.capitalize(), pet_age)
 
 # retrieve the pet’s name, type, and age and display this data on the screen
 print(f"""{Style.BRIGHT}\nUSER'S PET INFORMATION {Style.RESET_ALL} {Style.BRIGHT}

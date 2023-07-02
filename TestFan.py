c239372df46f65f7d7ba830fc6255c243041b55f
angelinetipa <angelinetipa@gmail.com>
2023-07-02 15:58:34 +0800

    second fan

--- TestFan.py
+++ TestFan.py
@@ -9,5 +9,11 @@ fan1.set_color("yellow")
 fan1.set_fanON()
 # Display first object speed, radius, color, on properties
 print(f"Speed: {fan1.get_speed()} \nRadius: {fan1.get_radius()} \nColor: {fan1.get_color()} \nOn: {fan1.get_on()}\n")
-# Secpnd object, assign the medium speed, radius 5, color blue, and turn it off
+
+# Second object, assign the medium speed, radius 5, color blue, and turn it off
+fan2 = Fan()
+fan2.set_speedMEDIUM()
+fan2.set_radius(5)
+fan2.set_color("blue")
+fan2.set_fanOFF()
 # Dsiplay second object speed, radius, color, on properties

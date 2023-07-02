81e9ed73fcbfee7c49efa88f50fd950adb3a85b1
angelinetipa <angelinetipa@gmail.com>
2023-07-02 14:37:56 +0800

    medium

--- FanClass.py
+++ FanClass.py
@@ -1,11 +1,14 @@
 # class named fan
-# constants named SLOW with value 1
-# constants named MEDIUM with value 2
-# constants named FAST with value 3
-# constructor that creates a fan with the specified speed (default SLOW), radius (default 5), color (default blue), and on (default False)
-# private int data field named speed 
-# private bool data field named on 
-# private float data field named radius 
-# private string data field named color 
-# getter methods
-# setter methods
+class Fan:
+    # constants named SLOW with value 1
+    SLOW = 1
+    # constants named MEDIUM with value 2
+    MEDIUM = 2
+    # constants named FAST with value 3
+    # constructor that creates a fan with the specified speed (default SLOW), radius (default 5), color (default blue), and on (default False)
+    # private int data field named speed 
+    # private bool data field named on 
+    # private float data field named radius 
+    # private string data field named color 
+    # getter methods
+    # setter methods

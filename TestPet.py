f78a6fa84ed53e18f8f4a5c7b8f27c2b75911b94
angelinetipa <angelinetipa@gmail.com>
2023-07-02 22:00:22 +0800

    object of pet class

--- TestPet.py
+++ TestPet.py
@@ -3,4 +3,5 @@ from PetClass import Pet
 from colorama import Style, Fore
 
 # creates an object of the class and prompts the user to enter the name, type, and age of his or her pet
+pet_name, pet_animal, pet_age = input("(Put comma between them.)\nEnter your pet's name, type and age: ")
 # retrieve the pet’s name, type, and age and display this data on the screen

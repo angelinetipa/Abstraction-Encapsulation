32527c7539c41cf87eb23ce54af6e4ceb0bc297f
angelinetipa <angelinetipa@gmail.com>
2023-07-02 17:38:14 +0800

    constructor

---
+++ CarClass.py
@@ -0,0 +1,10 @@
+# class named Car
+class Car:
+    # constructor that accepts the car’s year model, make as arguments, assign 0 to the speed 
+    def __init__(self, year, make, speed = 0):
+    # _ _year_model (for the car’s year model)
+    # _ _make (for the make of the car)
+    # _ _speed (for the car’s current speed)
+# accelerate method should add 5 to the speed data attribute each time it is called
+# brake method should subtract 5 from the speed data attribute each time it is called
+# get_speed method should return the current speed
